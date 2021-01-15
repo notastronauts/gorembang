f9defbfd5a0b1c898a0fef45cad38adc920b37c6
Nurul Arifin <arifinnurul957@gmail.com>
2021-01-15 07:32:42 +0700

    Setting up jetstream and admin dashboard

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html :class="{ 'theme-dark': dark }" x-data="data()" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <script src="{{asset('js/init-alpine.js')}}" defer></script>
+
+    @livewireStyles
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+
+<body>
+    <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
+        @include('layouts.menu')
+        @include('layouts.mobile-menu')
+
+        <div class="flex flex-col flex-1 w-full">
+            @include('layouts.navigation-dropdown')
+            <main class="h-full overflow-y-auto">
+                {{ $slot }}
+            </main>
+        </div>
+
+
+        @stack('modals')
+
+        @livewireScripts
+    </div>
+</body>
+
+</html>
